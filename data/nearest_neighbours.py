c87fbc3f8d1b5238527a6febc052c69764431398
ovacia345 <nielsvann@hotmail.nl>
2018-01-24 19:14:19 +0100

    Final script

--- data/nearest_neighbours.py
+++ data/nearest_neighbours.py
@@ -19,18 +19,20 @@ X_test = test_data["images"]
 y_test = test_data["labels"]
 
 accuracies = []
-for K in range(25,41):
+for K in range(1,31):
     neigh = nbrs.KNeighborsClassifier(n_neighbors = K, p = 2)
     neigh.fit(X_train, y_train)
     
     accuracies.append(neigh.score(X_test, y_test))
-    print(str(K) + ": " + str(accuracies[len(accuracies) - 1]))
+    print(str(K) + ": " + str(accuracies[-1]))
     
     
-# plt.scatter(range(1, 41), accuracies)
-# plt.title("Scatterplot of classification accuracy against number of nearest neighbours")
-# plt.xlabel("Number of nearest neighbours")
-# plt.ylabel("Accuracy")
-# plt.show()
+#plt.scatter(range(1, 31), accuracies)
+plt.scatter(range(1, 31), [0.9691, 0.9627, 0.9705, 0.9682, 0.9688, 0.9677, 0.9694, 0.967, 0.9659, 0.9665, 0.9668, 0.9661, 0.9653, 0.964, 0.9633,
+            0.9632, 0.963, 0.9633, 0.9632, 0.9625, 0.963, 0.9618, 0.9619, 0.9608, 0.9609, 0.9612, 0.9604, 0.9602, 0.9593, 0.9596])
+plt.title("Scatterplot of classification accuracy against number of nearest neighbours")
+plt.xlabel("Number of nearest neighbours")
+plt.ylabel("Accuracy")
+plt.show()
     
 
